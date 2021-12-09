2edfc525f5f20b4c112f49d60776a706dfc51656
Arjav jain <arjav.jain@arjavr020.successivetech.com>
2021-12-09 19:03:07 +0530

    task complete

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 import {errorHandler} from './libs/routes/errorHandler';
 import {notFound} from './libs/routes/notFoundRoute';
 import { default as MainRouter } from './router';
-
+import Database from './libs/Database';
+var DbUrl = require('dotenv').config().parsed.MONGO_URL;
 require('dotenv').config();
 var bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
 app.use(express.json());
+
  class Server{
     constructor(config){
-        app.listen(config);
-        this.run();
+        this.run(config);
     }
     setupRoutes(){
+        app.get("/", function  (req, res) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('Hello World!');
+            res.end();
+          });
         app.get('/health-check',function(req,res){
             res.send('I am Okay!');
         }); 
@@ -24,12 +30,11 @@ app.use(express.json());
         this.setupRoutes();
         this.initBodyParser();
     }
-    run(){
-        app.get("/",function (req, res) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write('Hello World!');
-            res.end();
-          });
+    async run(config){
+        const res  = await Database.open(DbUrl);
+        if (res){
+            app.listen(config);
+        }
     }
     initBodyParser(){
         app.use(bodyParser.urlencoded({
